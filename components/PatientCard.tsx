71803f465b5d32b3f2f3a9daa6ce13624effcb42
DANIEL <daniel.shamir@jonni.app>
2024-05-21 22:28:45 +0300

    Update PatientCard to fit the schema of the db

--- components/PatientCard.tsx
+++ components/PatientCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Card,
   CardHeader,
@@ -7,21 +9,12 @@ import {
 } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
-
 import { cn } from '@/lib/utils';
-
 import { Bell, Bed, Calendar, UserRound, Plus } from 'lucide-react';
+import {differenceInYears,differenceInDays } from 'date-fns';
+import { useState } from 'react';
+import { patientsTable } from '@/models/drizzle/patientsSchema';
 
-const mockPatientData = {
-  id: '4f64c137-b56c-49dc-a115-491b9b880c3d',
-  name: 'sick person',
-  age: 80,
-  socialId: '123456789', // social id
-  department: 'medical',
-  room: '420',
-  bed: '2',
-  addmissionDate: new Date(Date.now() - 1000000000),
-};
 
 const mockPatientTasks = [
   { urgent: false },
@@ -33,22 +26,22 @@ const mockPatientTasks = [
   { urgent: false },
 ];
 
-export const PatientCard = () => {
-  // get as prop
-  const { name, age, socialId, bed, department, room, addmissionDate } =
-    mockPatientData;
+const hospitalizationDays = (date: Date):number => differenceInDays(new Date() ,date);
+const age = (dateOfBirth: Date):number => differenceInYears(new Date() ,dateOfBirth);
+
+
+export const PatientCard = ({patient}: {patient: typeof patientsTable.$inferSelect}) => {
+  const { id,
+    firstName,
+    lastName,
+    unitName,
+    roomNumber} =
+  patient; 
+
   // get for every user? seems wasteful fetches how to improve?
   const tasks = mockPatientTasks;
 
-  const hospitalizationDays = (date: Date) => {
-    // milliseconds * seconds * minutes * hours
-    const millisInDay = 1000 * 60 * 60 * 24;
-    const millisFromDate = Date.now() - date.getTime();
-
-    return Math.round(millisFromDate / millisInDay);
-  };
-
-  const isUrgent = tasks.find((task) => task.urgent);
+  const isUrgent = tasks.some((task) => task.urgent);
 
   return (
     <Card className='w-80 sm:w-96'>
@@ -62,12 +55,12 @@ export const PatientCard = () => {
           <UserRound size='52px' />
           <div className='max-w-40 space-y-2 text-wrap break-words sm:max-w-52'>
             <CardTitle className=''>
-              <span>{name}</span>
-              {', '}
-              <span>{age}</span>
+              <span>{firstName + ' ' + lastName}</span>
+              {/* {', '}
+              <span>{age(date_of_birth)}</span> */}
             </CardTitle>
             <CardDescription className='text-white'>
-              <span>ID {socialId}</span>
+              <span>ID: {id}</span>
             </CardDescription>
           </div>
         </div>
@@ -82,24 +75,20 @@ export const PatientCard = () => {
       </CardHeader>
       <CardContent className='space-y-4 p-4 text-sky-700'>
         <p>{`${tasks.length ? tasks.length : 'no'} tasks`}</p>
-        <div className='flex gap-2'>
-          <Bed />
-          <p>{department}</p>
+        <div className='flex  gap-2'>
+          <p>{unitName}</p>
           <Separator orientation='vertical' className='h-auto' />
-          <p>Room {room}</p>
-
-          {bed && (
+          {roomNumber && (
             <>
-              <Separator orientation='vertical' className='h-auto' />
-              <p>Bed {bed}</p>
+              <Bed /><p>{roomNumber}</p>
             </>
           )}
         </div>
-        <div className='flex flex-col justify-between sm:flex-row'>
-          <div className='flex items-center gap-2'>
+        <div className='flex items-center justify-end'>
+          {/* <div className='flex items-center gap-2'>
             <Calendar className='self-start' />
-            <p>{`${hospitalizationDays(addmissionDate)} Days of hospitalization`}</p>
-          </div>
+            <p>{`${hospitalizationDays(admissionDate)} Days of hospitalization`}</p>
+          </div> */}
           <Button
             variant='ghost'
             className='flex items-center gap-1 self-end text-lg'
@@ -107,7 +96,7 @@ export const PatientCard = () => {
             <span>
               <Plus />
             </span>
-            Add task
+            New Task
           </Button>
         </div>
       </CardContent>
