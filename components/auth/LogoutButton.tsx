a3500c680a6443edbec12d895020c1f29d32f57c
Bora <b0rik556@gmail.com>
2024-05-22 20:55:37 +0300

    change logout button to server component

--- components/auth/LogoutButton.tsx
+++ components/auth/LogoutButton.tsx
@@ -1,13 +1,11 @@
-'use client';
-
 import { logoutUser } from '@/actions/auth';
 
 import { Button } from '@/components/ui/button';
 
 export const LogoutButton = () => {
-  const onLogoutClick = async () => {
-    await logoutUser();
-  };
-
-  return <Button onClick={onLogoutClick}>Logout</Button>;
+  return (
+    <form action={logoutUser}>
+      <Button>Logout</Button>
+    </form>
+  );
 };
