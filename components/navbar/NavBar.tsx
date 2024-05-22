a4a9b0581788433cc247662814f7ad15e2fe042b
Bora <b0rik556@gmail.com>
2024-05-22 12:17:54 +0300

    added navbar

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,14 +1,36 @@
+import Link from 'next/link';
+
+import { auth } from '@/auth';
+
+import { LogoutButton } from '@/components/auth/LogoutButton';
+import { NavLink } from '@/components/navbar/NavLink';
+
 import { Hospital } from 'lucide-react';
 
-export const NavBar = () => {
+const appName = 'Meditask Manager';
+
+export const NavBar = async () => {
+  const session = await auth();
+
   return (
-    <nav className='flex h-16 w-full items-center bg-sky-800 text-white'>
-      <div className='m-4 flex items-center gap-3'>
+    <nav className='flex h-16 w-full items-center justify-between bg-sky-800 px-4 text-white'>
+      <Link href='/' className='my-4 flex cursor-pointer items-center gap-3'>
         <Hospital className='h-10 w-10' />
-        <span className='text-xl font-bold tracking-wide'>
-          Meditask Manager
-        </span>
-      </div>
+        <span className='text-xl font-bold tracking-wide'>{appName}</span>
+      </Link>
+
+      <ul className='flex items-center gap-4'>
+        {session ? (
+          <>
+            <NavLink href='/'>Home</NavLink>
+            <NavLink href='/patients'>Patients</NavLink>
+            <NavLink href='/tasks'>Tasks</NavLink>
+            <LogoutButton />
+          </>
+        ) : (
+          <NavLink href='/auth/login'>Login</NavLink>
+        )}
+      </ul>
     </nav>
   );
 };
---
+++ components/navbar/NavLink.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+export const NavLink = ({ href, children }: NavLinkProps) => {
+  return (
+    <div>
+      <Link
+        className='rounded border bg-sky-800 px-4 py-2 hover:border-sky-500 hover:text-sky-200 '
+        href={href}
+      >
+        {children}
+      </Link>
+    </div>
+  );
+};
