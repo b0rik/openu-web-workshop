5ed55b97b5f72d8c2ed5073ab50b3a22eb060343
Bora <b0rik556@gmail.com>
2024-05-22 19:20:46 +0300

    login page redirect to root when already logged in

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,16 @@
+import { redirect } from 'next/navigation';
+
+import { auth } from '@/auth';
+
 import { LoginForm } from '@/components/auth/LoginForm';
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const session = await auth();
+
+  if (session) {
+    redirect('/');
+  }
+
   return (
     <div className='mx-auto max-w-md'>
       <LoginForm />
