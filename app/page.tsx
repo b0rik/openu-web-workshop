f50bd1e5be80efd58150b767e5d1885aed90124e
Bora <b0rik556@gmail.com>
2024-05-22 13:25:16 +0300

    homepage cleanup and tasks page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,11 @@
-import { TaskCard } from '@/components/TaskCard';
-import Link from 'next/link';
-
-import { auth, signOut } from '@/auth'; //test
+import { auth } from '@/auth'; //test
 
 const page = async () => {
   const session = await auth(); // test
 
-  if (!session) return <div>Not authenticated</div>; //test
   return (
-    <div className='flex flex-col items-center'>
-      <Link className='underline' href='/auth/login'>
-        Login page
-      </Link>
-      <Link className='underline' href='/auth/create-user'>
-        Create user page
-      </Link>
-      <Link className='underline' href='/patients/create'>
-        Create patient page
-      </Link>
-      <TaskCard />
-      <form //test
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <button type='submit'>Sign Out</button>
-      </form>
+    <div className='flex items-center justify-center'>
+      {session ? <p>Home page</p> : <p>Not authenticated</p>}
     </div>
   );
 };
---
+++ app/tasks/page.tsx
@@ -0,0 +1,8 @@
+const page = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <p>tasks page</p>
+    </div>
+  );
+};
+export default page;
