123c81977e48af7d9264a83a4f1c5f3f63ea306b
Bora <b0rik556@gmail.com>
2024-05-23 18:32:19 +0300

    added form submission indication

--- components/form/FormButton.tsx
+++ components/form/FormButton.tsx
@@ -1,12 +1,18 @@
+import { useFormState } from 'react-hook-form';
+
+import { LoaderCircle } from 'lucide-react';
+
 import { Button } from '@/components/ui/button';
 
 export const FormButton = ({ children }: { children: React.ReactNode }) => {
+  const { isSubmitting } = useFormState();
   return (
     <Button
       type='submit'
       className='w-full rounded-full bg-sky-500 text-lg hover:bg-sky-600'
+      disabled={isSubmitting}
     >
-      {children}
+      {isSubmitting ? <LoaderCircle className='animate-spin' /> : children}
     </Button>
   );
 };
