a89965662058056ff84ef9e78c9328d4453d9bc7
Bora <b0rik556@gmail.com>
2024-05-23 18:03:32 +0300

    added loading skeleton to forms that fetch data

--- components/patients-list/PatientsListSkeleton.tsx
+++ components/patients-list/PatientsListSkeleton.tsx
@@ -3,7 +3,6 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { ScrollArea } from '@/components/ui/scrollarea';
 
 export const PatientsListSkeleton = () => {
-  const dummies = Array(12).fill(0);
   return (
     <div className='rounded-xl bg-cyan-50 p-10'>
       <div className='flex justify-between'>
@@ -18,7 +17,7 @@ export const PatientsListSkeleton = () => {
 
       <ScrollArea className='h-screen rounded-md'>
         <div className='grid gap-6 lg:grid-cols-2 xl:grid-cols-3'>
-          {dummies.map((_, i) => (
+          {[...Array(12)].map((_, i) => (
             <Skeleton key={i} className='h-[230px] w-80 bg-sky-700 sm:w-96' />
           ))}
         </div>
