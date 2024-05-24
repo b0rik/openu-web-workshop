b904efa97fee68362f141fe7ca44da302db2a3ed
boris glushko <b0rik556@gmail.com>
2024-05-24 16:45:54 +0300

    form message transition

--- components/form/FormError.tsx
+++ components/form/FormError.tsx
@@ -2,10 +2,31 @@ interface FormErrorProps {
   message?: string;
 }
 
+const style = {
+  animation: 'open 0.20s linear',
+};
+
+const keyframes = `
+  @keyframes open {
+    from {
+      opacity: 0;
+
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
 export const FormError = ({ message }: FormErrorProps) => {
   return (
     message && (
-      <div className='rounded-md bg-red-600 p-3 text-white'>{message}</div>
+      <div>
+        <style>{keyframes}</style>
+        <div className='rounded-md bg-red-600 p-3 text-white' style={style}>
+          {message}
+        </div>
+      </div>
     )
   );
 };
--- components/form/FormSuccess.tsx
+++ components/form/FormSuccess.tsx
@@ -2,10 +2,31 @@ interface FormSuccessProps {
   message?: string;
 }
 
+const style = {
+  animation: 'open 0.20s linear',
+};
+
+const keyframes = `
+  @keyframes open {
+    from {
+      opacity: 0;
+
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
 export const FormSuccess = ({ message }: FormSuccessProps) => {
   return (
     message && (
-      <div className='rounded-md bg-green-400 p-3 text-black'>{message}</div>
+      <div>
+        <style>{keyframes}</style>
+        <div className='rounded-md bg-green-400 p-3 text-black' style={style}>
+          {message}
+        </div>
+      </div>
     )
   );
 };
