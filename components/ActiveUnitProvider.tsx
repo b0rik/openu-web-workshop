b07dac3d3c012c144e32987acf1404f8c905aa0b
boris glushko <b0rik556@gmail.com>
2024-05-30 10:40:27 +0300

    fix active unit context type

--- components/ActiveUnitProvider.tsx
+++ components/ActiveUnitProvider.tsx
@@ -1,21 +1,27 @@
 'use client';
 
-import { useState, createContext } from 'react';
+import { useState, createContext, Dispatch, SetStateAction } from 'react';
 
 type ActiveUnitProviderProps = {
   activeUnit: string | null | undefined;
   children: React.ReactNode;
 };
 
+type ActiveUnitContextType = {
+  currentActiveUnit: string | null | undefined;
+  setCurrentActiveUnit: Dispatch<SetStateAction<string | null | undefined>>;
+};
+
+export const ActiveUnitContext = createContext<ActiveUnitContextType>({
+  currentActiveUnit: null,
+  setCurrentActiveUnit: () => {},
+});
+
 export const ActiveUnitProvider = ({
   activeUnit,
   children,
 }: ActiveUnitProviderProps) => {
   const [currentActiveUnit, setCurrentActiveUnit] = useState(activeUnit);
-  const ActiveUnitContext = createContext({
-    currentActiveUnit,
-    setCurrentActiveUnit,
-  });
 
   return (
     <ActiveUnitContext.Provider
