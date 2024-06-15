4936b4f73964bef2db796df64471e3beb3284d34
boris glushko <b0rik556@gmail.com>
2024-06-15 20:27:39 +0300

    create task skeleton size fix

--- app/tasks/create/loading.tsx
+++ app/tasks/create/loading.tsx
@@ -2,7 +2,7 @@ import { CreateTaskSkeleton } from '@/components/create-task/createTaskSkeleton'
 
 const loading = () => {
   return (
-    <div>
+    <div className='mx-auto max-w-md md:max-w-3xl'>
       <CreateTaskSkeleton />
     </div>
   );
