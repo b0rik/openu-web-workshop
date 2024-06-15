99ffb16eada9a21dcf27db3e2a2fc7d9cb3665e3
boris glushko <b0rik556@gmail.com>
2024-06-15 20:26:45 +0300

    create task page loading skeleton

---
+++ app/tasks/create/loading.tsx
@@ -0,0 +1,10 @@
+import { CreateTaskSkeleton } from '@/components/create-task/createTaskSkeleton';
+
+const loading = () => {
+  return (
+    <div>
+      <CreateTaskSkeleton />
+    </div>
+  );
+};
+export default loading;
